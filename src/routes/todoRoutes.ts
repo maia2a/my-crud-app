bbf580c1c15311e230fab266dd2c2ceb105e820e
Gabriell Maia <88511880+maia2a@users.noreply.github.com>
2024-08-28 21:57:48 -0300

    adding another route
    I added another route for getting id of the specific "todo"

--- src/routes/todoRoutes.ts
+++ src/routes/todoRoutes.ts
@@ -4,6 +4,7 @@ import * as todoController from "../controller/todoController";
 const router = Router();
 
 router.get("/todos", todoController.getTodos);
+router.get("/todos/:id", todoController.getTodos);
 router.post("/todos", todoController.createTodo);
 router.delete("/todos/:id", todoController.deleteTodo);
 router.put("/todos/:id", todoController.updateTodo);
